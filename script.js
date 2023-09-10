9f7cd3e3c39b02bcba40e7a32b67227b13c6dc35
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2023-09-10 23:37:10 +0530

    Feat: onlick functionality for sidebar items

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
 const body = document.querySelector("body");
 const themeToggler = document.querySelector(".theme-toggler");
+const anchorElement = document.querySelector("a");
 
 menuBtn.addEventListener("click", () => {
   sideMenu.style.display = "block";
@@ -19,3 +20,12 @@ themeToggler.addEventListener("click", () => {
   themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
 });
+
+var list_items = document.querySelectorAll("a");
+var previousElement = "";
+for (var i = 1; i <= list_items.length; i++) {
+  list_items[i - 1].addEventListener("click", function toggle(i) {
+    this.classList.toggle("active");
+    this.previousElementSibling.classList.remove("active");
+  });
+}
